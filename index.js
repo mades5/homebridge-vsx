2eb35276cab7d2da1445b6fba01929d692b7574a
tickez <tickez@madnet.ch>
2020-01-25 18:08:50 +0100

    Refactorings, added timeout option

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var Service, Characteristic;
 var net = require('net');
 
+const QUERY_POWER = '?P\r\n';
+const QUERY_INPUT = '?F\r\n';
+const STATUS_POWER_OFF = "PWR1";
+const STATUS_POWER_ON = "PWR0";
+const REQUEST_POWER_OFF = 'PF\r\n';
+const REQUEST_POWER_ON = 'PO\r\n';
+const INPUT_PREFIX = "FN";
+const REQUEST_INPUT = INPUT_PREFIX + '\r\n';
+const TIMEOUT = 5000;
+
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
@@ -10,9 +20,13 @@ module.exports = function (homebridge) {
 function VSX(log, config) {
   this.log = log;
   this.name = config.name;
-  this.HOST = config.ip;
-  this.PORT = config.port;
-  this.INPUT = config.input;
+  this.ip = config.ip;
+  this.port = config.port;
+  this.input = config.input;
+  this.timeout = TIMEOUT;
+  if (config.timeout !== undefined) {
+    this.timeout = config.timeout;
+  }
 }
 
 VSX.prototype.getServices = function () {
@@ -22,57 +36,86 @@ VSX.prototype.getServices = function () {
 
   this.switchService = new Service.Switch(this.name);
   this.switchService.getCharacteristic(Characteristic.On)
-      .on('set', this.setOn.bind(this))
-      .on('get', this.getOn.bind(this));
+  .on('set', this.setOn.bind(this))
+  .on('get', this.getOn.bind(this));
 
   return [this.switchService, this.informationService];
 };
 
+function connectionErrorHandler(client, me, callback) {
+  client.on('error', function (ex) {
+    me.log("Received an error while communicating" + ex);
+    client.destroy();
+    callback(ex);
+  });
+  client.on('timeout', function () {
+    me.log("Received a timeout while communicating");
+    client.destroy();
+    callback({});
+  });
+}
+
 VSX.prototype.getOn = function (callback) {
 
   const me = this;
-  me.log('Query Power Status on '
-      + me.HOST + ':' + me.PORT + " input " + me.INPUT);
 
   var client = new net.Socket();
-  client.on('error', function (ex) {
-    me.log("Received an error while communicating" + ex);
-    callback(ex)
+  client.setTimeout(me.timeout);
+  connectionErrorHandler(client, me, callback);
+
+  // Query Power
+  client.connect(me.port, me.ip, function () {
+    me.log('Query Power Status on ' + me.ip + ':' + me.port + " input "
+        + me.input);
+    client.write(QUERY_POWER);
   });
 
-  client.connect(me.PORT, me.HOST, function () {
-    client.write('?P\r\n');
-  });
-
+  // Handle Response
   client.on('data', function (data) {
-    me.log('Received data: ' + data);
+    // me.log('Received data: ' + data);
 
-    var str = data.toString();
+    var response = data.toString();
 
-    if (str.includes("PWR1")) {
-      me.log("Power is Off");
+    function reportSwitchOff() {
+      me.log("Switch Off");
       client.destroy();
       callback(null, false);
-    } else if (str.includes("PWR0")) {
-      me.log("Power is On");
-      if (me.INPUT) {
-        client.write('?F\r\n'); // Request input
-      } else {
-        callback(null, true);
-        client.destroy();
-      }
-    } else if (str.includes("FN")) {
-      me.log("Current input is " + str);
+    }
+
+    function reportSwitchOn() {
+      me.log("Switch On")
       client.destroy();
-      if (str.includes(me.INPUT)) {
-        me.log("Current input matches target input of " + me.INPUT);
-        callback(null, true);
+      callback(null, true);
+    }
+
+    function queryInput() {
+      me.log("Power is On");
+      if (me.input) {
+        me.log("Setting input to " + me.input);
+        client.write(QUERY_INPUT); // Request input
+      } else {
+        reportSwitchOn();
+      }
+    }
+
+    function reportOnIfInputMatches() {
+      if (response.includes(me.input)) {
+        me.log("Receiver has correct input selected")
+        reportSwitchOn();
       } else {
         me.log("Receiver has different input selected");
-        callback(null, false);
+        reportSwitchOff()
       }
-    } else {
-      me.log("waiting");
+    }
+
+    if (response.includes(STATUS_POWER_OFF)) {
+      reportSwitchOff();
+    }
+    if (response.includes(STATUS_POWER_ON)) {
+      queryInput();
+    }
+    if (response.includes(INPUT_PREFIX)) {
+      reportOnIfInputMatches();
     }
   });
 };
@@ -81,33 +124,37 @@ VSX.prototype.setOn = function (on, callback) {
 
   const me = this;
   var client = new net.Socket();
-  client.on('error', function (ex) {
-    me.log("Received an error while communicating" + ex);
-    callback(ex)
-  });
+  client.setTimeout(me.timeout);
+  connectionErrorHandler(client, me, callback);
 
   if (on) {
-    client.connect(me.PORT, me.HOST, function () {
-      me.log('Set Power On on '
-          + me.HOST + ':' + me.PORT + " input " + me.INPUT);
-      client.write('PO\r\n');
-      if (me.INPUT == null) {
-        client.destroy();
-      }
+    // Request Power On
+    client.connect(me.port, me.ip, function () {
+      me.log('Set Power On on ' + me.ip + ':' + me.port + " input " + me.input);
+      client.write(REQUEST_POWER_ON);
     });
+
+    // Register Response Handler
     client.on('data', function (data) {
-      me.log("Change input to " + me.INPUT);
-      client.write(me.INPUT + 'FN\r\n');
+      if (me.input != null) {
+        me.log("Change input to " + me.input);
+        client.write(me.input + REQUEST_INPUT);
+      }
+      callback();
       client.destroy();
     });
   }
 
   if (!on) {
-    client.connect(me.PORT, me.HOST, function () {
-      me.log('Set Power Off on ' + me.HOST + ':' + me.PORT);
-      client.write('PF\r\n');
+    // Request Power Off
+    client.connect(me.port, me.ip, function () {
+      me.log('Set Power Off on ' + me.ip + ':' + me.port);
+      client.write(REQUEST_POWER_OFF);
+    });
+
+    client.on('data', function (data) {
+      callback();
       client.destroy();
     });
   }
-  callback();
 };
